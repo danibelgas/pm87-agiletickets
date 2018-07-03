3f0781777163bb720e57f3566b5dd28fc0dac69e
Robson <paniago@senado.leg.br>
2018-07-03 09:10:41 -0300

    Blah

--- TesteGit.java
+++ TesteGit.java
@@ -1,5 +1,5 @@
 public class TesteGit {
 	public static void main(String[] args) {
-		System.out.println("Teste de uso do GIT de novo");
+		System.out.println("Novo Teste de uso do GIT de novo");
 	}
 }
