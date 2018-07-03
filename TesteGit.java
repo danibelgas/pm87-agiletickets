50c35ac8a799bd42598a3d8d3121e69ee514e5ec
Robson <paniago@senado.leg.br>
2018-07-03 10:24:49 -0300

    Commit no branch

--- TesteGit.java
+++ TesteGit.java
@@ -1,5 +1,6 @@
 public class TesteGit {
 	public static void main(String[] args) {
-		System.out.println("Novo Teste de uso do GIT de novo");
+		System.out.println("Novo Teste 2 de uso do GIT de novo");
+		System.out.println("Testando branches");
 	}
 }
